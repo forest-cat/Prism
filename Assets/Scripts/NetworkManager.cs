d5a61eca6085a5d7dfbf460a1de13e44dce51aad
forest-cat <forest_cat@dismail.de>
2022-04-10 23:36:53 +0200

    Added: - Title Menu - Loading Menu - Error Menu - Room Menu - Create Room Menu - Platform - Baisc Pun Functionality

--- Assets/Scripts/NetworkManager.cs
+++ Assets/Scripts/NetworkManager.cs
@@ -1,18 +1,74 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
+using TMPro;
 
-public class NetworkManager : MonoBehaviour
+public class NetworkManager : MonoBehaviourPunCallbacks
 {
-    // Start is called before the first frame update
+    [SerializeField] TMP_InputField roomNameInputField;
+    [SerializeField] TMP_Text errorText;
+    [SerializeField] TMP_Text roomNameText;
+
     void Start()
     {
-        
+        Debug.Log("Connecting to Master");
+        PhotonNetwork.ConnectUsingSettings();
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void OnConnectedToMaster()
     {
-        
+        Debug.Log("Connected to Master");
+        PhotonNetwork.JoinLobby();
+    }
+
+    public override void OnJoinedLobby()
+    {
+        MenuManager.Instance.OpenMenu("TitleMenu");
+        Debug.Log("Joined Lobby");
+    }
+
+    public void CreateRoom()
+    {
+        if (string.IsNullOrEmpty(roomNameInputField.text))
+        {
+            return;
+        }
+        PhotonNetwork.CreateRoom(roomNameInputField.text);
+        Debug.Log("Created Room: " + roomNameInputField.text);
+        MenuManager.Instance.OpenMenu("LoadingMenu");
+    }
+
+    public override void OnJoinedRoom()
+    {
+        MenuManager.Instance.OpenMenu("RoomMenu");
+        Debug.Log("Joined Room: " + PhotonNetwork.CurrentRoom.Name);
+        roomNameText.text = PhotonNetwork.CurrentRoom.Name;
+    }
+
+    public override void OnCreateRoomFailed(short returnCode, string message)
+    {
+        errorText.text = "Room Creation Failed: " + message;
+        MenuManager.Instance.OpenMenu("ErrorMenu");
+        Debug.Log("Creating Room failed: " + message);
+    }
+
+    public void LeaveRoom()
+    {
+        PhotonNetwork.LeaveRoom();
+        MenuManager.Instance.OpenMenu("LoadingMenu");
+    }
+
+    public override void OnLeftRoom()
+    {
+        MenuManager.Instance.OpenMenu("TitleMenu");
+    }
+
+    public void QuitGame()
+    {
+        MenuManager.Instance.OpenMenu("LoadingMenu");
+        PhotonNetwork.Disconnect();
+        Debug.Log("Application ending after " + Time.time + " seconds");
+        Application.Quit();
     }
 }
